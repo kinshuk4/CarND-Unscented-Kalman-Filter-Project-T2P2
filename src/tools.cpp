76e0dc356976eb8b091eb97d4e10847edb19d75c
Kinshuk <kinshuk.ram@gmail.com>
2017-07-29 23:35:05 +0200

    Add rmse for ukf

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,37 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    /**
+    DONE:
+      * Calculate the RMSE here.
+    */
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+
+    if (estimations.size() == 0) {
+        std::cout << "CalculateRMSE() - Estimation size cannot be 0." << std::endl;
+        return rmse;
+    } else if (estimations.size() != ground_truth.size()) {
+        std::cout << "CalculateRMSE() - Invalid estimation or ground_truth data" << std::endl;
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    for (unsigned int i = 0; i < estimations.size(); ++i) {
+
+        VectorXd residual = estimations[i] - ground_truth[i];
+
+        //coefficient-wise multiplication
+        residual = residual.array() * residual.array();
+        rmse += residual;
+    }
+
+    //calculate the mean
+    rmse = rmse / estimations.size();
+
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
 }
